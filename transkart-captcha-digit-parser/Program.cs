a56bd651a3e82408559200362010b437953fa114
Дмитрий Храмов <thedcm@yandex.com>
2018-09-09 23:46:07 +0300

    Добавьте файлы проекта.

---
+++ transkart-captcha-digit-parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace transkart_captcha_digit_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
